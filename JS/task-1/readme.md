920e56c127fca7a2b3763581b69e1f2e5b8e9389
JOEL W <joelwalice@gmail.com>
2025-04-01 11:48:09 +0530

    Added Task-1 from JS

--- JS/task-1/readme.md
+++ JS/task-1/readme.md
@@ -1,7 +1,11 @@
 # Output for the To-Do Application 
 
+## Objective - Create a simple to-do list app where users can add new tasks, mark them as complete, and remove them.
+
+## Requirements - Use DOM for the CRUD Operations, Add Localstorage to store the data
+
 ## Output on adding the data through the HTML Input
-![alt text](image.png)
+![alt text](images/image.png)
 
 ## Output on getting the data through localStorage
-![alt text](image-1.png)
+![alt text](images/image-1.png)
--- JS/task-1/todo.js
+++ JS/task-1/todo.js
@@ -1,11 +1,46 @@
-let data = [];
+let data = JSON.parse(localStorage.getItem("data")) || [];
 const p = document.getElementById("output");
 
-p.innerText = "Data from Localstorage : \n \n" +  localStorage.getItem("data").split(",").join("\n");
+function updateDisplay() {
+    p.innerHTML = "";
+    if (data.length > 0) {
+        const header = document.createElement("h3");
+        header.innerText = "Data from LocalStorage:";
+        p.append(header);
+    }
 
-function addValue(){
-    const value = document.getElementById("value").value;
-    data.push(value);
-    p.innerText = data.join('\n');
-    localStorage.setItem("data", data);
+    // Display each task with toggle button
+    data.forEach((item, index) => {
+        const div = document.createElement("div");
+        div.style.marginBottom = "10px"; 
+
+        const textSpan = document.createElement("span");
+        textSpan.innerText = `${index + 1}. ${item.value} - `;
+
+        const btn = document.createElement("button");
+        btn.innerText = item.task ? "Completed" : "Not Completed";
+        btn.style.cursor = "pointer";
+        btn.style.marginLeft = "10px";
+        btn.onclick = () => toggleTask(index);
+
+        div.append(textSpan);
+        div.append(btn);
+        p.append(div);
+    });
 }
+
+function addValue() {
+    const value = document.getElementById("value").value;
+    if (value.trim() === "") return; 
+    data.push({ value, task: false });
+    localStorage.setItem("data", JSON.stringify(data)); 
+    updateDisplay();
+}
+
+function toggleTask(index) {
+    data[index].task = !data[index].task;
+    localStorage.setItem("data", JSON.stringify(data));
+    updateDisplay();
+}
+
+updateDisplay();
